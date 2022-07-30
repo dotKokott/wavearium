8bbf836b7ad028de0856a1904d4f0c26086a37ca
Christian Kokott <christian.kokott@gmail.com>
2022-07-30 14:32:36 +0200

    boilerplate svelte

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wavearium
 WaveEdit like wavetable editor for viewing, searching, creating and collecting of wavetables. Focusing on the Piston Honda MKIII format for now.
+
+
+## Technology decision
+- Svelte or Vue3 for frontend
+- express or fastapi for backend
+- JS or TS
+
+### Considerations
+This is a learning project but I also want there to be some ease to it.
+I will look at svelte ts
---
+++ client/src/App.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	export let name: string;
+
+	let list = [
+		{
+			name: "John",
+			age: 30
+		},
+		{
+			name: "Jane",
+			age: 25
+		},
+		{
+			name: "Bob",
+			age: 20
+		}
+	];
+
+	function setName(newName: string) {
+		name = newName;
+	}
+
+	// computed
+	// $: doubled = count * 2
+
+	// watch
+	// $: if(count % 2 == 0) { console.log(' do something ') }
+
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={() => setName('chango')}>Change name</button>
+
+	{#if name != 'world'}
+		<p>{name} is not world</p>
+	{/if}
+
+	<ul>
+		{#each list as person, i}
+			<li>{person.name} is {person.age} years old</li>
+		{/each}
+	</ul>
+
+	<!--
+		{#await promise}
+			<p>...waiting</p>
+		{:then number}
+			<p>The number is {number}</p>
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}		
+	-->
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
